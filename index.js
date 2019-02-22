70e1bee7480400520fd39556eb427c33358aa269
Jason Bamford <scarfandgoggles@yahoo.co.uk>
2019-02-22 22:12:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-var kittens = [] //define your array here
+var kittens = [ 'Milo', 'Otis', 'Garfield' ];
+
+function destructivelyAppendKitten(kitteh) {
+  kittens.push(kitteh)
+}
+
+function destructivelyPrependKitten(haz) {
+  kittens.unslice(haz)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()  /* were any kittens harmed? */
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.slice(1)  /* please stop! */
+}
+
+function appendKitten(cheez) {
+  return [...kittens, cheez]
+}
+
+function prependKitten(burger) {
+  return [burger, ...kittens]
+}
+
+function removeLastKitten() {
+  
+}
+
 
 // Add your functions and code here
