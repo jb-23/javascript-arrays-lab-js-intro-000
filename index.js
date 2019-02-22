db23c308453864debf25f0c2d1b9dd33b86bc1b8
Jason Bamford <scarfandgoggles@yahoo.co.uk>
2019-02-22 22:15:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ function prependKitten(burger) {
 }
 
 function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
 
+function removeFirstKitten() {
+  return kittens.slice(1)
 }
 
 
